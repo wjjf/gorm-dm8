4b7a064bb6bf7e024d5aaacb94ab96a0b231327c
hao.wei <weihao>
2022-02-17 19:55:15 +0800

    fix:hastable

--- migrator.go
+++ migrator.go
@@ -5,7 +5,6 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 	"gorm.io/gorm/migrator"
-	"strings"
 )
 
 type Migrator struct {
@@ -165,7 +164,7 @@ func (m Migrator) HasIndex(value interface{}, name string) bool {
 		return m.DB.Raw(
 			"SELECT COUNT(*) FROM USER_INDEXES WHERE TABLE_NAME = ? AND INDEX_NAME = ?",
 			m.Migrator.DB.NamingStrategy.TableName(stmt.Table),
-			strings.ToUpper(name),
+			m.Migrator.DB.NamingStrategy.IndexName(stmt.Table, name),
 		).Row().Scan(&count)
 	})
 
